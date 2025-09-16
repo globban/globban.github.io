3c92e7497eb4c9bf20752626a0ddadf5f59ac2c0
globban <algot.fun@gmail.com>
2025-09-16 19:51:57 +0200

    update

---
+++ cast/sender/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // Basic: just pass through for now
+  event.respondWith(fetch(event.request));
+});
